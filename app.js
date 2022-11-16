43283909d7a545002848f71a817452c26f77bd27
aliak629918 <aliak629918@hotmail.com>
2022-11-16 11:26:59 +0000

    ticket 5 get api/articles/article_id completed

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticles } = require("./controllers/articles.controller");
+const {
+  getArticles,
+  getArticleId,
+} = require("./controllers/articles.controller");
 
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
+
+app.get("/api/articles/:article_id", getArticleId);
 
 app.use("/*", (req, res) => {
   res.status(404).send({ msg: "Path not found!" });
@@ -15,7 +20,6 @@ app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
-    console.log(err);
     req.status(400).send({ msg: "Internal Server Error" });
   }
 });
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,21 @@
-const { fetchArticles } = require("../models/articles.model");
+const { fetchArticles, fetchArticleById } = require("../models/articles.model");
 
 exports.getArticles = (req, res, next) => {
   const { sort_by, order, topic } = req.query;
   fetchArticles(sort_by, order, topic)
     .then((articles) => {
       res.status(200).send({ articles: articles });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article: article });
     })
     .catch((err) => {
       next(err);
